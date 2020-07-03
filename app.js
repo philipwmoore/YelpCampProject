01836c6484c1d885258e3c0822cf2f5dec0fe985
Philip Moore <philipwmoore@gmail.com>
2020-07-03 04:14:03 +0000

    change app.listen to not have hardcoded port

--- app.js
+++ app.js
@@ -52,6 +52,7 @@ app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen(3000, function() { 
-  console.log('Server listening on port 3000'); 
-});
+var port = process.env.PORT || 3000;
+    app.listen(port, function () {
+      console.log("Server Has Started!");
+    });
